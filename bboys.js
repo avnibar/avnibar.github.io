765a858d6f2c52b3ddac38ed16f529d5d7ee6f42
avnibar <18222@gdc.school.nz>
2021-04-08 20:43:30 +1200

    yoo

---
+++ bboys.js
@@ -0,0 +1,11 @@
+function placeOrder() {
+    var orderForm = document.getElementById("my-form");
+    results = "<h3>Success!</h3> Here is your order.";
+    results += "<br>Name: " + orderForm.elements["my-name"].value;
+    results += "<br>Address: " + orderForm.elements["my-address"].value;
+    results += "<br>I like to order: " + orderForm.elements["my-drink"].value;
+    results += "<br>Quantity: " + orderForm.elements["my-qty"].value;
+    var orderResults = document.getElementById("my-order");
+    orderResults.style.display = "block";
+    orderResults.innerHTML = results;
+}
